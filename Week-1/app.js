15b753077de99a0433d30933e8ec6eaa4a91d757
Abhijeet Sharma <abhijeetsharma476@gmail.com>
2022-08-21 04:51:31 +0530

    added weel-1 task

---
+++ Week-1/app.js
@@ -0,0 +1,16 @@
+const array = [0,1,2,3,4,5,6,7,8,'A','B','C','D','E','F','a','b','c','d','e','f'];
+const btn = document.querySelector('.btn');
+const bgColor = document.querySelectorAll('.bg-color');
+const hexCode = document.querySelectorAll('.hex-code');
+
+btn.addEventListener('click', getHex);
+function getHex(){
+    let newHexCode = ['#'];
+    for (let i = 0; i<6; i++){
+        let randomHex = Math.floor(Math.random()*array.length);
+        newHexCode = newHexCode + array[randomHex];
+        console.log(newHexCode);       
+    }
+    bgColor.forEach(e => e.style.backgroundColor = newHexCode);
+    hexCode.forEach(e => e.innerHTML = newHexCode);
+}
